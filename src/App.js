db8b286cd208b7cbb181b1f87d2dee5ebc57b0d0
joko19 <jokoslamet459@gmail.com>
2022-08-04 21:10:29 +0700

    section 2

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
                   <div className="md:text-7xl text-2xl font-bold  w-full">Take back control of your hairline</div>
                   <p className="text-lg my-4">Stop, reverse and prevent hair loss, with the right treatment delivered to your door.</p>
                   <button className="text-white bg-orange-500 font-bold text-lg py-1 my-4 rounded-full px-8">Start your Free visit</button>
-                  <div className="flex justify-between my-4">
+                  <div className="flex justify-between my-4 text-xs md:text-sm">
                     <div className="flex flex-col md:flex-row gap-2">
-                      <img src="/asset/icons/checklist.svg" alt="checklist" className="w-4 h-4 mx-auto md:my-auto" />
+                      <img src="/asset/icons/checklist.svg" alt="checklist" className="w-4 h-4 mx-auto md:my-auto " />
                       <div>
                         <p>Works for</p>
                         <p>9/10 men</p>
@@ -59,7 +59,26 @@ function App() {
                 <img src="/asset/images/pharmacy.png" alt="icon medExpress" className="my-auto" />
               </div>
             </div>
-            <img src="/asset/images/background.png" className="w-full" />
+            <img src="/asset/images/background.png" className="w-full" alt="background" />
+          </div>
+        </div>
+        <div className="bg-blue-900 h-72 md:hidden" />
+      </section>
+
+      <section>
+        <div className="flex gap-8 md:gap-12 m-8 justify-center relative flex-end justify-items-end">
+          <div className="h-full h-full flex-col justify-between ">
+            <img src="/asset/icons/google.svg" alt="google" />
+            <p className="text-blue-700 text-sm text-center font-bold">Rating:  4.9/5</p>
+          </div>
+          <div className="">
+            <img src="/asset/icons/rating-desktop.svg" alt="rating" className="hidden md:block" />
+            <img src="/asset/icons/rating-mobile.svg" alt="rating" className="md:hidden" />
+            <p className="text-blue-700 text-sm text-center font-bold">Rating:  4.8/5</p>
+          </div>
+          <div className="my-auto hidden md:block">
+            <img src="/asset/icons/reviewsio.svg" alt="review" />
+            <p className="text-blue-700 text-sm text-center font-bold">Rating:  4.77/5</p>
           </div>
         </div>
       </section>
